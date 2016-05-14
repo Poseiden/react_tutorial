f1539d0623486c4e6fbd251934a4ffc9f82a8566
HaiboZhu <hbzhu@thoughtworks.com>
2016-05-14 21:19:11 +0800

    feature complete but not optimization

--- public/scripts/tutorial02.js
+++ public/scripts/tutorial02.js
@@ -1,14 +1,117 @@
+var CommentList = React.createClass({
+	render: function() {
+		var commentNodes = this.props.data.map(function(comment){
+			return (
+					<Comment author={comment.author} key={comment.id} >
+						{comment.text}
+					</Comment>
+				);
+		});
+		return (
+			<div className = 'commentList'>
+				{commentNodes}
+			</div>
+			);
+	}
+});
+var CommentForm = React.createClass({
+	getInitialState: function() {
+		return {author: '',text: ''};
+	},
+	handleAuthorChange: function(e) {
+		this.setState({author:e.target.value});
+	},
+	handleTextChange: function(e) {
+		this.setState({text:e.target.value});
+	},
+	handleSubmit: function(e) {
+		e.preventDefault();
+		var author = this.state.author.trim();
+		var text = this.state.text.trim();
+		if(!author || !text){
+			return;
+		};
+		this.props.onCommentSubmit({author: author,text: text});
+		this.setState({author:'',text:''});
+	},
+	render: function() {
+		return (
+				<form className="commentForm" onSubmit={this.handleSubmit}>
+					<input type='text' placeholder="Your name" value={this.state.author} onChange={this.handleAuthorChange} /> 
+					<input type='text' placeholder="Say something...." value={this.state.text} onChange={this.handleTextChange} />
+					<input type='submit' value="Post" />
+				</form>
+			);
+	}
+});
+var Comment = React.createClass({
+	rawMarkup: function() {
+		var rawMarkup = marked(this.props.children.toString(),{sanitize:true});
+		return { __html: rawMarkup };
+	},
+
+	render: function() {
+		return (
+			<div className='comment'>
+				<h2 className='commentAuthor'>
+					{this.props.author}
+				</h2>
+				<span dangerouslySetInnerHTML={this.rawMarkup()} />
+			</div>
+		);
+	}
+});
+// var data =[
+// 	{id:1,author:'yuexinlin',text:'woshiyigewangbadan'},
+// 	{id:2,author:'zhuhaibo',text:'loushangshiwangbadan'}
+// ];
 var CommentBox = React.createClass({
+	loadCommentsFromServer: function() {
+		$.ajax({
+			url:this.props.url,
+			dataType:'json',
+			cache:false,
+			success:function(data) {
+				this.setState({data:data});
+			}.bind(this),
+			error: function(xhr,status,err) {
+				console.error(this.props.url,status,err.toString());
+			}.bind(this)
+		});
+	},
+	getInitialState: function() {
+		return {data:[]};
+	},
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer,this.props.pollInterval);
+	},
+	handleCommentSubmit: function(comment) {
+		$.ajax({
+			url:this.props.url,
+			type:'POST',
+			dataType:'json',
+			data:comment,
+			success: function(data) {
+				this.setState({data:data});
+			}.bind(this),
+			error: function(xhr,status,err) {
+				console.error(this.props.url,status,err.toString());
+			}.bind(this)
+		});
+	},
 	render: function() {
 		return (
 			<div className="commentBox">
-				Hello , this is comment box ! 
+				<h2>this is comment box</h2>
+				<CommentList data={this.state.data}/>
+				<CommentForm onCommentSubmit={this.handleCommentSubmit}/>
 			</div>
 		);
 	}
 });
 
 ReactDOM.render(
-	<CommentBox />,
+	<CommentBox url='/api/comments' pollInterval={2000}/>,
 	document.getElementById('content')
 );
